a349b0f6f68b2a058292e5f4a93e8b1748b60ff5
ByeongWooCho <whquddn55@gmail.com>
2021-03-08 18:36:23 +0900

    Modify hierarchical of downloads folder and git inited in downloads folder to upload downloaded files to github

--- app.js
+++ app.js
@@ -2,27 +2,61 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface( {
+    input: process.stdin,
+    output: process.stdout
+});
 
-const userid = 'whquddn55'
-const userpassword = 'whxnxl55;;'
+let userid = process.argv[2];
+let userpassword = process.argv[3];
 
 function timer(ms) {
     return new Promise(res => setTimeout(res, ms));
 }
 
+async function saveLog(msg) {
+    console.log(msg);
+    fs.appendFileSync('./log', msg + '\n', 'utf8');
+}
+
+async function saveConfig(problemIds) {
+    let previousIds = await loadConfig();
+    previousIds.push(problemIds);
+    previousIds.sort();
+    fs.writeFileSync('./previous.json', JSON.stringify(previousIds), 'utf8');
+}
+
+async function loadConfig() {
+    let previousIds = [];
+    if (fs.existsSync('./previous.json'))
+        previousIds = JSON.parse(fs.readFileSync('./previous.json', 'utf8'));
+    return previousIds;
+}
+
 async function getProblemIds(page) {
-    return await page.evaluate(() => {
+    let previousIds = await loadConfig();
+    let temp = await page.evaluate(() => {
         let data = document.getElementsByClassName("panel-body")[0].children;
         let problemIds = []
         for (let i = 0; i < data.length; ++i)
             problemIds.push(data[i].innerText);
         return problemIds;
     });
+
+    let problemIds = [];
+    for (let pId of temp) {
+        if (previousIds.includes(pId))
+            continue;
+        problemIds.push(pId);
+    }
+    saveLog(`${problemIds.length} New Problems Found!`);
+    return problemIds;
 }
 
 async function saveSource(page, sourceLink) {
     await page.goto(sourceLink);
-    console.log(`WebPage landed at ${sourceLink}`);
+    saveLog(`WebPage landed at ${sourceLink}`);
 
     let source = await page.evaluate(() => document.getElementsByName("source")[0].innerText);
 
@@ -38,27 +72,45 @@ async function saveSource(page, sourceLink) {
     })
 
     let problemId = infoArray[2].value;
-    console.log(`Estimated Problem Id : ${problemId}`);
+    saveLog(`Estimated Problem Id : ${problemId}`);
 
-    fs.writeFileSync(`./downloads/${problemId}.cpp`, source, "utf8");
+    if(!fs.existsSync('./downloads'))
+        fs.mkdirSync('./downloads');
+
+    let secondaryFolder = problemId * 1 - (problemId * 1) % 1000;
+    if(!fs.existsSync(`./downloads/${secondaryFolder}`))
+        fs.mkdirSync(`./downloads/${secondaryFolder}`);
+
+    fs.writeFileSync(`./downloads/${secondaryFolder}/${problemId}.cpp`, source, "utf8");
     fs.writeFileSync(
-        `./downloads/${problemId}.json`, 
+        `./downloads/${secondaryFolder}/${problemId}.json`, 
         JSON.stringify(infoArray).replace(/},/gi, '},\n\t').replace('[','[\n\t').replace('}]','}\n]')
         , "utf8"
     );
-    console.log(`File writed ${problemId}.cpp And ${problemId}.json`);
+    saveLog(`File writed ${problemId}.cpp And ${problemId}.json`);
+    saveConfig(problemId);
+    await gitCommit(problemId);
 
+    let time = 1000 * Math.floor(Math.random() * 20) + 10000;
+    console.log(`waiting ${time}ms`);
     // wait 10~30 seconds random to avoid block account and avoid server overload
-    await timer(1000 * Math.floor(Math.random() * 20) + 10000);
+    await timer(time);
 }
 
 async function getSources(page) {
     let problemIds = await getProblemIds(page);
-    console.log(problemIds, problemIds.length);
 
     for (let i = 0; i < problemIds.length; ++i) {
+        let secondaryFolder = problemIds[i] * 1 - (problemIds[i] * 1) % 1000;
+        if (fs.existsSync(`./downloads/${secondaryFolder}/${problemIds[i]}.cpp`)){
+            saveConfig(problemIds[i]);
+            continue;
+        }
+
         await page.goto(`https://www.acmicpc.net/status?from_mine=1&problem_id=${problemIds[i]}&user_id=${userid}`);
-        console.log(`[#${i}]Problem ${problemIds[i]}`);
+
+        saveLog(`[#${i}]Problem ${problemIds[i]}`);
+
         let sourceLink = await page.evaluate(() => {
             let tr = document.getElementById("status-table").getElementsByTagName('tbody')[0].getElementsByTagName('tr');
             let ret;
@@ -76,8 +128,50 @@ async function getSources(page) {
         await saveSource(page, sourceLink);
     }
 }
+
+
+async function checkLoginInfo() {
+    async function inputId() {
+        process.stdout.write("ID : ");
+        await new Promise(resolve => 
+            rl.on("line", function(line) {
+            userid = line;
+            rl.close();
+            resolve();
+        }));
+    }
+
+    async function inputPw() {
+        process.stdout.write("PW : ");
+        await new Promise(resolve => 
+            rl.on("line", function(line) {
+            userpassword = line;
+            rl.close();
+            resolve();
+        }));
+    }
+
+    if (process.argv.length < 4) {
+        if (process.argv.length == 2) 
+            await inputId();
+        await inputPw();
+        console.log(1);
+    }
+}
+
+async function gitCommit(problemId) {
+    const exec = require('child_process').exec;
+    exec(`cd downloads && git add . && git commit -m "${problemId}.cpp Added"`);
+}
+
+async function pushToGit() {
+    const exec = require('child_process').exec;
+    exec('cd downloads && git push origin master');
+}
  
-const doPuppeteer = async() => {
+async function doPuppeteer() {
+    await checkLoginInfo();
+
     const browser = await puppeteer.launch({
         headless : false
     });
@@ -98,7 +192,13 @@ const doPuppeteer = async() => {
     await page.waitForNavigation();
     
     await getSources(page);
+
+    browser.close();
+    console.log('done');
+
+    await pushToGit();
+
+    process.exit();
 }
- 
- 
+
 doPuppeteer();
